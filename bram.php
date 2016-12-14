e58cb5bb59a3af5f76d5ef9c7a19d90a8b01ed06
frits191 <mauricemol@hotmail.nl>
2016-12-14 09:35:49 +0100

    Rick (#1)
    * maurice.php
    
    subpagina maurice
    
    * bram.php
    
    subpagina Bram
    
    * dennis.php
    
    subpagina Dennis
    
    * nbpoject
    
    * julian.php
    
    subpagina julian
    
    * subpagina's
    
    subpagina's van jasper, rick en stan
    
    * stylesheet
    
    stylesheet voor alle pagina's
    
    * stylesheet

---
+++ bram.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Portfolio Bram</title>
+    </head>
+    <body>
+    </body>
+</html>
