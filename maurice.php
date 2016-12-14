b16d3ec611fb62cedefbe0690bdf34ffd030821b
Rickmarges <rickmarges@gmail.com>
2016-12-14 09:00:49 +0100

    maurice.php
    subpagina maurice

---
+++ maurice.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Portfolio Maurice</title>
+    </head>
+    <body>
+    </body>
+</html>
