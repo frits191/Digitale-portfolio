c06ead546a7c9bf7e2701c96957b575ddd060dc5
frits191 <mauricemol@hotmail.nl>
2016-12-14 09:58:10 +0100

    mysqli functions
    Added a connect and execute query function

--- functions.php
+++ functions.php
@@ -1,8 +1,37 @@
 <?php 
 
 class functions {
-    function example() {
-        echo "hallo";
+    function connect() {
+        $db_name = "digitaalportfolio";
+        $DBconnect = mysqli_connect("localhost", "INF1H", "stenden");
+        //connect to the database
+        if ($DBconnect === FALSE) {
+            echo "<p>Unable to connect to the database server.</p>"
+            . "<p>Error code " . mysqli_errno() . ": "
+            . mysqli_error() . "</p>";
+        } else {
+            //select the database
+            $db = mysqli_select_db($DBconnect, $db_name);
+            if ($db === FALSE) {
+                echo "<p>Unable to connect to the database server.</p>"
+                . "<p>Error code " . mysqli_errno() . ": "
+                . mysqli_error() . "</p>";
+                mysqli_close($DBconnect);
+                $return;
+            }
+        }
+        return $DBconnect;
+    }
+    
+        $DBconnect = $this->connect();
+        $QueryResult = mysqli_query($DBconnect, $SQLString);
+        if ($QueryResult === false) {
+            echo "Error excecuting query.<br>" . mysqli_errno($DBconnect) . ": " . mysqli_error($DBconnect);
+            mysqli_close($DBconnect);
+            return;
+        }
+        mysqli_close($DBconnect);
+        return $QueryResult;
     }
 }
 
