2ba58b0d5b1e24436de9b0abc765bbc8ccc691bd
LumaLux <jasper.folkers@student.stenden.com>
2016-12-15 12:24:59 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <?php
+    session_start();
+    require ('core/database.php');
+    if(isset($_GET['page']))
+    {
+    $page = $_GET['page'];
+    }else{
+    $page = "home";
+    }
+    ?>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+    <?php   require ('layout/header.php');?>
+    <div id="container">
+    <div id="content">
+    <?php
+    if(file_exists("pages/$page.php"))
+    {
+    require ("pages/$page.php");
+    }else{
+    echo "<img class='img-responsive center-block' src='img/404.png' alt='page not found'>";
+    }
+    ?>
+    </div></div>
+<?php require ('layout/footer.php');?>
