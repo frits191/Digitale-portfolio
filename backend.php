1d5359dfa5bd525e950451b372ac6f3713b82e39
frits191 <mauricemol@hotmail.nl>
2016-12-22 10:50:24 +0100

    Small changes to header and backend

--- backend.php
+++ backend.php
@@ -4,19 +4,19 @@ echo "<div id='container'>";
 	echo "<div id='content'>";
 		require ('core/layout/headerbackend.php');
 
-		if (isset($_SESSION["ID"])) {
-			if (isset($SESSION["email"])) {
-				$ID = $_SESSION["ID"];
-				$email = $_SESSION["email"];
+		if (isset($_SESSION["id"])) {
+			if (isset($SESSION["e-mail"])) {
+				$ID = $_SESSION["id"];
+				$email = $_SESSION["e-mail"];
 			} else {
-				header ('Location: index.php?page=home');
-				exit();
+				//header ('Location: index.php?page=home');
+				//exit();
 			}
 		} else {
-			header ('Location: index.php?page=home');
-			exit();
+			//header ('Location: index.php?page=home');
+			//exit();
 		}
-
+		
 
 
 	echo "</div>";
--- core/layout/headerbackend.php
+++ core/layout/headerbackend.php
@@ -5,24 +5,21 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="header.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
 <?php
         echo "<body>";
         echo "<div id='header'>";
-            echo "<div id='mainblock'>";
-           
-           
-                echo "<img id='logo' src='../images/header-logo.png' alt='logo'/>";
-               
+            echo "<div id='mainblock'>";  
+                echo "<img id='logo' src='../images/header-logo.png' alt='logo'/>";          
             echo "</div>";
             echo "<div id='userblock'>";
                echo " <p>";
                     echo "U bent ingelogd als:<br/>";
                     echo "(Sample text)<br/>";
-                    echo "<br/>             "       ;
+                    echo "<br/>";
                     echo "<a id='userlink' href=''>Uitloggen</a>";
                 echo "</p>";
             echo "</div>";
