96ef789fd23eb07b2fbb841280388fa629e25efd
frits191 <mauricemol@hotmail.nl>
2016-12-22 09:53:39 +0100

    Maarten de lange merge (#5)
    * spellingsfout
    
    * Maarten pagina aanmaak
    
    * header

--- core/layout/header.php
+++ core/layout/header.php
@@ -1,12 +1,43 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-	<head>
-		<title></title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	</head>
-	<body>
+    <head>
+        <title>Digitaal Portfolio</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css">-->
+        <link rel="stylesheet" type="text/css" href="header.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div id="header">
+            <div id="mainblock">
+			
+			
+                <img id='logo' src='../images/header-logo.png' alt='logo'/>
+			   
+            </div>
+            <div id="userblock">
+                <p>
+                    U bent ingelogd als:<br/>
+                    (Sample text)<br/>
+                    <br/>                    
+                    <a id="userlink" href="">Uitloggen</a>
+                </p>
+            </div>
+            <div id="downblock">
+                <div class="item-add">
+                     <img src="../images/header-maptoevoegen.png" alt="map"/> 
+                </div>
+                <div class="item-add">
+                    <img src="../images/header-itemtoevoegen.png" alt="item"/>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
