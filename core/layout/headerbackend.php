8be3158171d2e96849c06c68b244642a8532bdb4
frits191 <mauricemol@hotmail.nl>
2017-01-13 02:09:34 +0100

    Backend - Backend pages, get capture system, admin register function
    It' s 2AM I should probably sleep

--- core/layout/headerbackend.php
+++ core/layout/headerbackend.php
@@ -11,28 +11,41 @@
 			<div class='col-lg-3'>
 				<div id='userblock'>
 					<p>
-						U bent ingelogd als:
-						<br />
-						(Sample text)
-						<br />
-						<br />
-						<a id='userlink' href='backend.php?p=logout'>Uitloggen</a>
+						<?php
+						if (isset($_SESSION["loggedIn"])) {
+							if ($_SESSION["loggedIn"] == true) {
+								echo "U bent ingelogd als: <br />";
+								echo $_SESSION["name"] . " ("  . $_SESSION["role"] . ")";
+								echo "<br /><br />";
+								echo "<a id='userlink' href='backend.php?p=logout'>Uitloggen</a>";
+							} else {
+								echo "U bent niet ingelogd.<br>";	
+							}
+						} else {
+							echo "U bent niet ingelogd.<br>";
+						}
+						?>
 					</p>
 				</div>
 			</div>
 		</div>
 		<div class='row'>
-			<div id='downblock' class='col-lg-12'>
-				<div class='item-add'>
-					<div class='btn-group'>
-						<button type="button" class="btn btn-default" aria-label="Left Align">
-							<span class="glyphicon glyphicon-file" aria-hidden="true" title='Add a new file'></span>
-						</button>
-						<button type="button" class="btn btn-default" aria-label="Left Align">
-							<span class="glyphicon glyphicon-folder-open" aria-hidden="true" title='Add a new folder'></span>
-						</button>
-					</div>
-				</div>
+			<div id='downblock' class='col-lg-12'> 
+			<?php
+			if ($p == "projecten") {
+				//Bootstrap Modal for adding a new project folder and possibly adding files
+				echo "<div class='item-add'>";
+					echo "<div class='btn-group'>";
+						echo "<button type='button' class='btn btn-default' aria-label='Left Align'>";
+							echo "<span class='glyphicon glyphicon-file' aria-hidden='true' title='Add a new file'></span>";
+						echo "</button>";
+						echo "<button type='button' class='btn btn-default' aria-label='Left Align'>";
+							echo "<span class='glyphicon glyphicon-folder-open' aria-hidden='true' title='Add a new folder'></span>";
+						echo "</button>";
+					echo "</div>";
+				echo "</div>";
+			}
+			?>
 			</div>
 		</div>
 	</div>
